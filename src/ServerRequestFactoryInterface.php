2c5ddea0785b4c9e48dfe53c18db043575dc160d
Rasmus Schultz <mindplay-dk@users.noreply.github.com>
2017-05-13 16:43:33 +0200

    Unified factory-interface
    Per comments in #31

--- src/ServerRequestFactoryInterface.php
+++ src/ServerRequestFactoryInterface.php
@@ -12,20 +12,9 @@ interface ServerRequestFactoryInterface
      *
      * @param string $method
      * @param UriInterface|string $uri
+     * @param array $server optional server params ($_SERVER or similar structure)
      *
      * @return ServerRequestInterface
      */
-    public function createServerRequest($method, $uri);
-
-    /**
-     * Create a new server request from server variables.
-     *
-     * @param array $server Typically $_SERVER or similar structure.
-     *
-     * @return ServerRequestInterface
-     *
-     * @throws \InvalidArgumentException
-     *  If no valid method or URI can be determined.
-     */
-    public function createServerRequestFromArray(array $server);
+    public function createServerRequest($method, $uri, $server = []);
 }
