9969b78f8dacb8970fd78db5617de9b7eb776d68
Rasmus Schultz <mindplay-dk@users.noreply.github.com>
2017-05-16 21:43:01 +0200

    Update ServerRequestFactoryInterface.php

--- src/ServerRequestFactoryInterface.php
+++ src/ServerRequestFactoryInterface.php
@@ -10,6 +10,10 @@ interface ServerRequestFactoryInterface
     /**
      * Create a new server request.
      *
+     * Note that server-params are taken precisely as given - no parsing/processing
+     * of the given values is performed, and, in particular, no attempt is made to
+     * determine the HTTP method or URI, which must be provided explicitly.
+     *
      * @param string $method
      * @param UriInterface|string $uri
      * @param array $server optional server params ($_SERVER or similar structure)
